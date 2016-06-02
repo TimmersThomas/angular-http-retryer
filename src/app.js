9c4666bb8c721e10ed64d9f828437dfa63f61ff0
Florent Gouget <florent.gouget@gmail.com>
2016-06-02 14:30:07 +0200

    whatever

--- src/app.js
+++ src/app.js
@@ -10,33 +10,71 @@
   function HttpRetryerProvider () {
     var self = this
     self.maxRetries = 10
-    self.delay = 30000
+    self.delay = 600000
     self.retries = 0
+    self.timeout = 5000
+    self.pending = 0
 
     self.setMaxRetries = function (max) {
       self.maxRetries = max
     }
 
-    this.$get = [ '$injector', '$timeout', '$q', function ($injector, $timeout, $q) {
+    self.setTimeout = function (timeout) {
+      self.timeout = timeout
+    }
+
+    self.setDelay = function (delay) {
+      self.delay = delay
+    }
+
+    this.$get = [ '$injector', '$timeout', '$q', '$rootScope', function ($injector, $timeout, $q, $rootScope) {
       var retryer = {
         resetRetries: function () {
           self.retries = 0
+          retryer.emit()
           $timeout(retryer.resetRetries, self.delay)
         },
 
+        checkPending: function(){
+          if (!self.pending)
+            self.retries = 0
+          retryer.emit()
+        },
+
         request: function (config) {
+          self.pending++
+          config.timeout = self.timeout
           return config
         },
 
         response: function (response) {
+          self.pending --
+          retryer.checkPending()
           return response
         },
 
+        emit: function(){
+          if (self.retries)
+            $rootScope.$broadcast('RequestRetry', self.retries)
+          else
+            $rootScope.$broadcast('NetworkBackToNormal')
+          if (self.retries > self.maxRetries/4){
+            $rootScope.$broadcast('BadNetwork', self.retries)
+          }
+        },
+
         responseError: function (rejection) {
-          if (rejection.status === 0 && ++self.retries <= self.maxRetries) {
+          retryer.checkPending()
+          if (rejection.status <= 0 && ++self.retries <= self.maxRetries) {
+            retryer.emit()
+            var delay = 1000 * self.retries
             var $http = $injector.get('$http')
-            return $http(rejection.config)
+            return $timeout(function () {
+              self.pending --
+              return $http(rejection.config)
+            }, delay)
           } else {
+            self.pending --
             return $q.reject(rejection)
           }
         }
